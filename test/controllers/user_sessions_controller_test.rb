a27884c1893fcbabf7dd97730a5fb1688a05cd7b
leonden <leon.hochwimmer@breitband.ch>
2023-10-26 13:48:42 +0200

    Start testing

--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -1,13 +1,26 @@
-require "test_helper"
+require 'test_helper'
 
 class UserSessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get user_sessions_new_url
-    assert_response :success
+  setup do
+    @user = users(:one)
   end
 
-  test "should get create" do
-    get user_sessions_create_url
-    assert_response :success
+  test "should create session" do
+    post user_sessions_url, params: { user: { name: @user.name, password: "ba4m4n1ngh0ng"} }
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_equal session[:user_id], User.find_by(name: @user.name).id
+  end
+
+  test "should destroy session" do
+    post user_sessions_url, params: { user: { name: @user.name, password: "ba4m4n1ngh0ng"} }
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_equal session[:user_id], User.find_by(name: @user.name).id
+
+    delete user_session_url(id: session[:user_id]), headers: { "HTTP_AUTHORIZATION" => @authorization }
+    assert_response :redirect
+    assert_redirected_to root_path
+    assert_nil session[:user_id]
   end
 end
